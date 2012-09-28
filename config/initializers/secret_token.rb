299b9dfd6eddeefb2a5246a2232632d35d79c6b0
marclameu <marclameu@gmail.com>
2012-09-27 22:06:06 -0300

    Inicializando o repositório

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BolaoOnline::Application.config.secret_token = 'ebb546be18863e2f88268d77def43429825baf045a32b6b1e7b2377aba1cb7f999ad718ff930e862ee09b2ed5448fc8974055551e0d94ad93b698a5a6b14a807'
