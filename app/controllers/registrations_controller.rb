3754b02719f546a167d33125221ba47516741a56
Marcelino Lameu <marcelino@marcelinux.(none)>
2012-10-18 01:42:09 -0300

    Melhorias na regra de apostas, inclusão de times na tela de usuário (incompleto)

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,24 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    @teams = Team.all
+    super
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        #redirect_to :
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    super
+  end
+end
