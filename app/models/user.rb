9b7512e0ee9fe08f25a40dc8d0f936e86eed9b50
marclameu <marclameu@gmail.com>
2012-10-29 22:14:20 -0200

    testes para correção do login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,21 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #campos requeridos
-  validates_presence_of :name, :Cpf, :teams
-  
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :born_date, :email, :name, :image, :Cpf
-
+ 
+  #campos requeridos
+  validates_presence_of :name, :Cpf
+  #validates_associated :teams
+  
   #associações
   has_many :gambles
   has_and_belongs_to_many :teams
   has_many :participations
   has_many :championships, :through => :participations
-  accepts_nested_attributes_for :teams
+  #accepts_nested_attributes_for :teams
 
   # atributo imagem
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "300x180>"}
+ 
 end
