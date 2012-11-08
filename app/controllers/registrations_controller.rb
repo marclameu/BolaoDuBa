93243e7ddf2e13dbd5e2bedf05c2f6255b60da84
marclameu <marclameu@gmail.com>
2012-11-07 23:36:17 -0200

    Inserido associação de usuário a campeonato na tela de cadastro

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,33 +1,44 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
     @teams = Team.all
+    @championships = Championship.all
     resource = build_resource({})
     respond_with resource
   end
 
   def create
-    #resource = build_resource({})
-    build_resource
-    if params[:team][:id] == ""
-      resource.errors.add("", "Escolha seu time")
-      respond_with resource 
+    resource = build_resource({})
+    #build_resource
+    resource.errors.add("", "Escolha seu time") if params[:team][:id] == ""
+    resource.errors.add("", "Voce deve selecionar um campeonato") if params[:championship][:id] == ""
+    if resource.errors.empty? #and current_user.present?
+       super   
+       current_user.teams << Team.find(params[:team][:id]) if current_user.present?
+       current_user.participations << Participation.new(:championship_id => params[:championship][:id].to_i) if current_user.present?
     else
-      super   
-      current_user.teams << Team.find(params[:team][:id]) if current_user.present?
+       respond_with resource
     end
-    
-    #if (params[:team][:id] == nil) or (params[:team][:id] == "")
-    #  flash[:erro] = "Selecione seu time" 
-    #  redirect_to sign_up_path
-    #else
-    #  super
-    #  current_user.teams << Team.find(params[:teams][:id]) if current_user.present?
-    #end
-    #resource.errors.add(:teams, "Selecione seu time!") if params[:teams][:id] == nil
-	  #current_user.teams << Team.find(params[:teams][:id]) if current_user.present?
   end
 
+  def edit
+    render :edit
+  end
+  
   def update
-    super
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    if resource.update_with_password(resource_params)
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
   end
 end
